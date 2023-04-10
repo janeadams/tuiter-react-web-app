6d9c4e9862d7143ec3767b3e6d75a7981c25d523
Jane Adams <jane@universalities.com>
2023-04-09 21:22:46 -0400

    a9 Mongo fa22

--- public/tuiter/HomeScreen/NavigationSidebar/SideNavListItem.js
+++
@@ -1,9 +0,0 @@
-const SideNavListItem = (navItem) => {
-  const activeClass = navItem.active ? 'active' : ''
-  console.log(navItem, activeClass)
-   return (`
- <a class="list-group-item ${activeClass}" href="${navItem.url}">
-            <i class="${navItem.icon}"></i><span class="d-none d-xl-inline">  ${navItem.name}</span></a>
-`); }
-
-export default SideNavListItem;
